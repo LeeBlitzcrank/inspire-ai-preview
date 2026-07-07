449afcbe9a084139879c842cae8e8f55306142d2
blitzcrank <cn.blitzcrank@gmail.com>
2026-07-07 12:25:02 +0800

    init GitHub Pages 部署 & 生产环境穿透配置

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { fileURLToPath } from 'url'
-import path from 'path'
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
   base: '/',
   plugins: [vue()],
   resolve: {
-    alias: { '@': path.resolve(__dirname, 'src') }
+    alias: { '@': './src' }
   },
   server: {
     proxy: {
